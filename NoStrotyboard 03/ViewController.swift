4c7f76273877bfe28f6d3f4142057a4380490b63
정순우 <JungSoonwoo@jeongsun-uui-MacBook-Air.local>
2018-03-08 15:45:01 +0900

    Initial Commit

---
+++ NoStrotyboard 03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoStrotyboard 03
+//
+//  Created by 정순우 on 2018. 3. 8..
+//  Copyright © 2018년 정순우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
